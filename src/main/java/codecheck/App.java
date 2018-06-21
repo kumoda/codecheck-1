c4eb62896b874115eaffff0cae3a1b33ddd1b4e8
kumoda.sho <kumoda.sho@intra.vpjr.jp>
2018-06-21 17:34:28 +0900

    暫定

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,28 @@
 package codecheck;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class App {
-	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
-		}
-	}
+    public static void main(String[] args) {
+        String apiUrl = "/api/hash";
+        URL connectUrl;
+        try {
+            connectUrl = new URL(apiUrl);
+
+            HttpURLConnection con = (HttpURLConnection)connectUrl.openConnection();
+
+            con.setRequestMethod("GET");
+            con.setDoOutput(true);
+            con.setInstanceFollowRedirects(true);
+
+            System.out.println("レスポンスヘッダ:");
+            System.out.println("レスポンスコード[" + con.getResponseCode() + "] " +
+                                        "レスポンスメッセージ[" + con.getResponseMessage() + "]");
+        } catch (IOException e) {
+            // TODO 自動生成された catch ブロック
+            e.printStackTrace();
+        }
+    }
 }
